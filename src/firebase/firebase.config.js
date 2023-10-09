65a5f6017ef963935ab58d388d3a67de5b8d188b
brcshakil1 <brcshakil72@gmail.com>
2023-10-09 18:27:02 +0600

    added .env.local file and hide firebase config file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBWRzeXhES9x6JvleFee9uJyA1N-SyDmC8",
-  authDomain: "eduevent-39570.firebaseapp.com",
-  projectId: "eduevent-39570",
-  storageBucket: "eduevent-39570.appspot.com",
-  messagingSenderId: "35022033919",
-  appId: "1:35022033919:web:658a9e0399f8f62b6c0dca",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
